ab09fe94f02fec8f0b471f6a6e167f1cf43b8822
DarukBey <dorukersoy2006@gmail.com>
2023-01-25 18:05:45 +0300

    Update elementify.js

--- js/elementify.js
+++ js/elementify.js
@@ -1,14 +1,48 @@
 function formatValid(p, elementArray, result) {
-    p.innerHTML += "Name(s):" + elementArray.map(item => item.Name).join(" ");
-    p.innerHTML += " (" + elementArray.map(item => item.Symbol).join("") + ")";
+    let numberSum = 0
+    let massSum = 0;
+    for (let item of elementArray) {
+        numberSum += item.atomicNumber;
+        massSum += item.atomicMass;
+    }
+    
+    p.classList.add("valid");
+    p.classList.add("message")
+
+    p.innerHTML += "What an ELEMENTIFYABLE name. <br></br> Here's what I got: <br></br> ";
+    p.innerHTML += "<b>Name(s):</b> " + elementArray.map(item => item.Name).join(" ");
+    p.innerHTML += "<br></br> <b>Symbols: </b>" + elementArray.map(item => item.Symbol).join("");
+    p.innerHTML += "<br></br> <b>Total Atomic Number: </b>" + elementArray.map(item => item.atomicNumber).join(" + ") + " = " +numberSum;
+    p.innerHTML += "<br></br> <b>Total Atomic Mass: </b>" + elementArray.map(item => item.atomicMass).join(" + ") + " = " +massSum;
     result.appendChild(p);
 }
 
 function formatInvalid(p, elementArray, result) {
-    p.innerHTML += "Invalid Input";
-    p.innerHTML += "Name(s):" + elementArray.map(item => item.Name).join(" ");
-    p.innerHTML += " (" + elementArray.map(item => item.Symbol).join("") + ")";
-    result.appendChild(p);
+    if (elementArray.length > 0) {
+        
+        let numberSum = 0
+        let massSum = 0;
+        for (let item of elementArray) {
+            numberSum += item.atomicNumber;
+            massSum += item.atomicMass;
+        }
+
+        p.classList.add("zort");
+        p.classList.add("message")
+
+        p.innerHTML += "The name is incomplete but don't worry everybody has flaws. <br></br> Here's what I got: <br></br> ";
+        p.innerHTML += "<b>Name(s):</b> " + elementArray.map(item => item.Name).join(" ");
+        p.innerHTML += "<br></br> <b>Symbols: </b>" + elementArray.map(item => item.Symbol).join("");
+        p.innerHTML += "<br></br> <b>Total Atomic Number: </b>" + numberSum;
+        p.innerHTML += "<br></br> <b>Total Atomic Mass: </b>" + massSum;
+        result.appendChild(p);
+    } else {
+        p.classList.add("invalid");
+        p.classList.add("message")
+
+        p.innerHTML = "Write a better name my man! Can't find anything :/";
+        result.appendChild(p);
+    }
 }
 
 function elementify() {
@@ -26,16 +60,13 @@ function elementify() {
 
             //Clear the Text
             result.innerHTML = "";
-            
+
             //Diving Input into Array
             const inputArray = [...input];
 
             //Defining Variables
             let index = 0;
             let p = document.createElement('p');
-            p.classList.add("p");
-            p.classList.add("highlight");
-
 
             while (index < inputArray.length) {
                 if (inputArray.length - index > 2) {
@@ -45,7 +76,8 @@ function elementify() {
                     if (filtered.length > 0) {
                         elementArray.push(filtered[0]);
                         index += 2;
-                    } else {
+                    }
+                    else {
                         let str = inputArray[index];
                         let filtered = data.filter(item => item.Symbol.toLowerCase() == str.toLowerCase());
                         if (filtered.length > 0) {
@@ -57,7 +89,8 @@ function elementify() {
                             break;
                         }
                     }
-                } else {
+                }
+                else {
                     if (index == inputArray.length - 1) {
                         let str = inputArray[index];
                         let filtered = data.filter(item => item.Symbol.toLowerCase() == str.toLowerCase());
@@ -75,25 +108,27 @@ function elementify() {
                         let twostr = inputArray[index] + inputArray[index + 1];
                         let filtered = data.filter(item => item.Symbol.toLowerCase() == twostr.toLowerCase());
 
-                    if (filtered.length > 0) {
-                        elementArray.push(filtered[0]);
-                        index += 2;
-                        formatValid(p, elementArray, result);
-                    } else {
-                        let str = inputArray[index];
-                        let filtered = data.filter(item => item.Symbol.toLowerCase() == str.toLowerCase());
                         if (filtered.length > 0) {
                             elementArray.push(filtered[0]);
-                            index += 1;
+                            index += 2;
                             formatValid(p, elementArray, result);
                         }
                         else {
-                            formatInvalid(p, elementArray, result);
-                            break;
+                            let str = inputArray[index];
+                            let filtered = data.filter(item => item.Symbol.toLowerCase() == str.toLowerCase());
+                            if (filtered.length > 0) {
+                                elementArray.push(filtered[0]);
+                                index += 1;
+                                formatValid(p, elementArray, result);
+                            }
+                            else {
+                                formatInvalid(p, elementArray, result);
+                                break;
+                            }
                         }
-                    }
                     }
                 }
             }
-        });
+    });
 }
+
